14abfabd2d73458eb88d35e6f1e79fa1d71718e6
dxren <dorothy.x.ren@gmail.com>
2024-08-20 15:34:40 -0400

    types and controller routes written for books

--- backend/controller/user.ts
+++ backend/controller/user.ts
@@ -1,29 +1,44 @@
 import { Router } from "express";
-import { CreateUserBody, CreateUserResponse, DeleteCurrentUserResponse, GetCurrentUserResponse, UpdateCurrentUserBody, UpdateCurrentUserResponse } from '../../shared/apiTypes';
-import { processRequest } from 'zod-express-middleware';
+import {
+  CreateUserBody,
+  CreateUserResponse,
+  DeleteCurrentUserResponse,
+  GetCurrentUserResponse,
+  UpdateCurrentUserBody,
+  UpdateCurrentUserResponse,
+} from "../../shared/apiTypes";
+import { processRequest } from "zod-express-middleware";
 
 export const userRouter = Router();
 
 // CREATE_USER
-userRouter.post('/', processRequest({body: CreateUserBody}), (req, res) => {
-    const result: CreateUserResponse = {message: `Create user: ${req.body.message}`};
-    res.json(result);
+userRouter.post("/", processRequest({ body: CreateUserBody }), (req, res) => {
+  const result: CreateUserResponse = {
+    message: `Create user: ${req.body.message}`,
+  };
+  res.json(result);
 });
 
 // GET_CURRENT_USER
-userRouter.get('/', (_, res) => {
-    const result: GetCurrentUserResponse = {message: 'Get current user'};
-    res.json(result);
+userRouter.get("/", (_, res) => {
+  const result: GetCurrentUserResponse = { message: "Get current user" };
+  res.json(result);
 });
 
 // UPDATE_CURRENT_USER
-userRouter.put('/', processRequest({body: UpdateCurrentUserBody}), (req, res) => {
-    const result: UpdateCurrentUserResponse = {message: `Update current user: ${req.body.message}`};
+userRouter.put(
+  "/",
+  processRequest({ body: UpdateCurrentUserBody }),
+  (req, res) => {
+    const result: UpdateCurrentUserResponse = {
+      message: `Update current user: ${req.body.message}`,
+    };
     res.json(result);
-});
+  }
+);
 
 // DELETE_CURRENT_USER
-userRouter.delete('/', (_, res) => {
-    const result: DeleteCurrentUserResponse = {message: `Delete current user`};
-    res.json(result);
+userRouter.delete("/", (_, res) => {
+  const result: DeleteCurrentUserResponse = { message: `Delete current user` };
+  res.json(result);
 });
